6af9ba1e579d5ef8cb3174c2d7366ca27b3270e5
Yu <yufeng.physics@gmail.com>
2021-08-20 23:52:43 -0400

    first commit

---
+++ DukePPM/src/main/java/io/yuintelligence/dukeppm/DukePpmApplication.java
@@ -0,0 +1,13 @@
+package io.yuintelligence.dukeppm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DukePpmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DukePpmApplication.class, args);
+    }
+
+}
---
+++ DukePPM/src/test/java/io/yuintelligence/dukeppm/DukePpmApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yuintelligence.dukeppm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DukePpmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
