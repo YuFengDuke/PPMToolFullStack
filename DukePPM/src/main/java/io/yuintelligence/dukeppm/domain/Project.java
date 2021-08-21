b9dbdbd20815615a1d02038a58920124f3b9be9b
Yu <yufeng.physics@gmail.com>
2021-08-21 01:03:00 -0400

    H2 database

--- DukePPM/src/main/java/io/yuintelligence/dukeppm/domain/Project.java
+++ DukePPM/src/main/java/io/yuintelligence/dukeppm/domain/Project.java
@@ -1,4 +1,97 @@
 package io.yuintelligence.dukeppm.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date start_data;
+    private Date end_date;
+
+    private Date created_At;
+    private Date update_At;
+
+    public Project() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStart_data() {
+        return start_data;
+    }
+
+    public void setStart_data(Date start_data) {
+        this.start_data = start_data;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
+    }
+
+    public Date getCreated_At() {
+        return created_At;
+    }
+
+    public void setCreated_At(Date created_At) {
+        this.created_At = created_At;
+    }
+
+    public Date getUpdate_At() {
+        return update_At;
+    }
+
+    public void setUpdate_At(Date update_At) {
+        this.update_At = update_At;
+    }
+
+    @PrePersist
+    protected  void onCreat(){
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.update_At = new Date();
+    }
+
+
 }
